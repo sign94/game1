a5b27295ea5fb848126af0ce64286377986df022
Piotr Janik <piotrekj11@gmail.com>
2020-02-06 18:01:14 +0100

    add playerMove

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
